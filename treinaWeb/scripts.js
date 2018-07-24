76ab8f100d6e7151348fb0403e08a3f279a4b33a
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-07-24 20:58:52 -0300

    Update scripts.js

--- treinaWeb/scripts.js
+++ treinaWeb/scripts.js
@@ -48,7 +48,7 @@ if (first_click) {
       		document.body.style.background = "#fff";
 		document.getElementById("singlebutton").style.color = "#fff";
 		first_click = true;
-		rythm.stop();
+		rythm.stop(freeze);
     }
 	localStorage.setItem('clique', first_click)
   
